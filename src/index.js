c29eac45693d5498fc4653a5208827b8e025846c
jeyden94 <jeyden94@gmail.com>
2025-09-21 22:47:47 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
-import odinImage from "./images/odin.png";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 
-console.log(greeting);
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const error = document.getElementById("error");
 
-const image = document.createElement("img");
-image.src = odinImage;
+// Regular expression for email validation as per HTML specification
+const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
 
-document.body.appendChild(image);
+// Check if the email is valid
+
+
+// Update email input class based on validity
+
+
+// Update error message and visibility
+
+
+// Handle input event to update email validity
+
+
+// Handle form submission to show error if email is invalid
+
+
+// Now we can rebuild our validation constraint
+// Because we do not rely on CSS pseudo-class, we have to
+// explicitly set the valid/invalid class on our email field
+
+
+// This defines what happens when the user types in the field
+
+// This defines what happens when the user tries to submit the data
