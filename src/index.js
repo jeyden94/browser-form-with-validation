f5ab520b7d8ea39c5a3a88642ee3d924fe061d3c
jeyden94 <jeyden94@gmail.com>
2025-09-28 19:42:18 +0000

    Updated code for readability, added comments

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 import "./styles.css";
 
+// eslint-disable-next-line no-undef
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
 
+//-- Set Variables to Access Form, Input Divs and Error Message Divs --//
 const form = document.querySelector("form")
 
 const email = document.getElementById("email")
 const emailError = document.getElementById("email-error")
 
-const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+//-- Add Regular Expression Variables for Each Input --//
+const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ 
+//-- Dynamically Update Input Validity and Generate Error Messages --//
 const isValidInput = (input, regExp) => {
   const validity = input.value.length !== 0 && regExp.test(input.value);
   return validity;
 };
 
-const setInputClass = (input, isValid) => {
-  input.className = isValid ? "valid" : "invalid";
+const setInputClass = (inputDiv, validityStatus) => {
+  inputDiv.className = validityStatus ? "valid" : "invalid";
 };
 
-const updateError = (input, error, isValid) => {
-  if (isValid) {
-    error.textContent = "";
-    error.removeAttribute("class");
+const updateError = (thisInput, thisError, thisValidityStatus) => {
+  if (thisValidityStatus) {
+    thisError.textContent = "";
+    thisError.removeAttribute("class");
   } else {
-    if (input === email) {
-      error.textContent = "I expect an email, darling!";
-      error.setAttribute("class", "active");
+    if (thisInput === email) {
+      thisError.textContent = "I expect an email, darling!";
+      thisError.setAttribute("class", "active");
     }
   }
 };
 
-const handleInput = (input, error, regExp) => {
-  const validity = isValidInput(input, regExp);
-  setInputClass(input, validity);
-  updateError(input, error, validity);
+const handleInput = (inputValue, errorMessage, inputRegExp) => {
+  const validity = isValidInput(inputValue, inputRegExp);
+  setInputClass(inputValue, validity);
+  updateError(inputValue, errorMessage, validity);
 };
 
+
+//-- Form-Level Event Handler That Considers All Inputs --//
 const handleSubmit = (event) => {
   event.preventDefault();
 
@@ -46,10 +53,11 @@ const handleSubmit = (event) => {
   updateError(email, emailError, emailValidity);
 };
 
+//-- Initialize Each Input's Validity --//
 const emailValidity = isValidInput(email, emailRegExp);
 setInputClass(email, emailValidity);
 
-
+//-- Add Event Listeners for Each Input --//
 email.addEventListener("input", () => handleInput(email, emailError, emailRegExp));
 
 form.addEventListener("submit", handleSubmit);
